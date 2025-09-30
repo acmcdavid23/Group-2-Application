ce8ede43954401835ce47701e3a83c60b6829127
Gunba <GunballZ@outlook.com>
2025-09-29 20:29:20 -0500

    Final Commit

--- obj/Debug/net9.0/InternApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/InternApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InternApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10e771bf1d30968017f916591f9ad0c01f933ae7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+591e134934ecb3e63b11cc2254fc02d31faf8192")]
 [assembly: System.Reflection.AssemblyProductAttribute("InternApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InternApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
